0d6e8656ff56aa5b7db6d3815924b8b191928ce5
nnoromiv <nnorom.prince44@gmail.com>
2024-02-24 14:16:05 +0100

    Advanced Contract. Secured Contract

--- migrations/1_deployer_contract.js
+++ migrations/1_deployer_contract.js
@@ -1,6 +1,5 @@
 const Faucet = artifacts.require("Faucet")
 
 module.exports = async function(deployer){
-    // Replace  0x8B573DFd9120A434f13A65dADaeca00885e33108 with your personal address
-    await deployer.deploy(Faucet, "0x8B573DFd9120A434f13A65dADaeca00885e33108")
+    await deployer.deploy(Faucet, __Address__ )
 }
